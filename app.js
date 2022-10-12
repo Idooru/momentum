e4e8e213e7eaa6e998ec2de79eb1e05003253530
idooru <shere1765@gmail.com>
2022-10-12 20:49:09 +0900

    [추가] const와 let 사용하기

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const a = 5;
 const b = 2;
-const myName = "nico";
+let myName = "nico";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "nicolas";
+
+console.log("your new name is " + myName);
+var c = 5;
+var c = 6;
