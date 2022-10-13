9e234e1f5a48fb8e828f89c1de13fc7b02d0d927
idooru <shere1765@gmail.com>
2022-10-13 21:37:30 +0900

    [수정] 더욱더 개선된 방법으로 클래스 이름을 변경하여 글자 색깔 변경

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const h1 = document.querySelector(".hello h1");
 
 h1.addEventListener("click", () => {
-  const clickedClass = "clicked";
-  h1.className = h1.className === clickedClass ? "" : clickedClass;
+  h1.classList.toggle("clicked");
 });
