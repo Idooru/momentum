9eb58c065282c42c64ae2dc5e603d353a131abbd
idooru <shere1765@gmail.com>
2022-10-13 21:21:27 +0900

    [수정] 개선된 방법으로 클래스 이름 변경

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const h1 = document.querySelector(".hello h1");
 
 h1.addEventListener("click", () => {
-  const currentColor = h1.style.color;
-  let newColor;
-
-  if (currentColor === "blue") {
-    newColor = "tomato";
-  } else {
-    newColor = "blue";
-  }
-
-  h1.style.color = newColor;
+  const clickedClass = "clicked";
+  h1.className = h1.className === clickedClass ? "" : clickedClass;
 });
