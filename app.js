2d09101938b5dfd55aa843a7285aea3910bcd1b8
idooru <shere1765@gmail.com>
2022-10-13 20:52:56 +0900

    [수정] 자바스크립트에서 CSS 사용하기 1

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector(".hello h1");
 
-console.dir(h1);
+h1.addEventListener("click", () => {
+  const currentColor = h1.style.color;
+  let newColor;
 
-function handleTitleClick() {
-  h1.style.color = "blue";
-}
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
 
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copier!");
-}
-
-function handleWindowOffline() {
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-  alert("ALL GOOD");
-}
-
-h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+  h1.style.color = newColor;
+});
