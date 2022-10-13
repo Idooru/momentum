00636fe923908995ed625cd6a7c2c3474db0541a
idooru <shere1765@gmail.com>
2022-10-13 09:09:19 +0900

    [수정] 함수 사용하기 2

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-function sayHello() {
-  console.log("Hello my name is C");
+function plus(firstNumber, secondNumber) {
+  console.log(firstNumber + secondNumber);
 }
 
-sayHello("nico");
-sayHello("dal");
-sayHello("lynn");
+function divide(a, b) {
+  console.log(a / b);
+}
+
+plus(8, 60);
+divide(98, 20);
+
+function sayHello(otherPersonName) {
+  console.log("hello! " + otherPersonName + "nice to meet you!");
+}
+
+const player = {
+  name: "nico",
+  sayHello,
+};
+
+console.log(player.name);
+player.sayHello("lynn");
