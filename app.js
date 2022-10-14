61d9e51e19241d31e1b1f60ebf96171079209d38
idooru <shere1765@gmail.com>
2022-10-14 19:00:15 +0900

    [수정] 폼 제출

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 
 const onLoginClick = () => {
-  console.log("hello" + loginInput.value);
+  const username = loginInput.value;
+  if (username === "") {
+    alert("Please write your name!!");
+  } else if (username.length > 15) {
+    alert("Your name is too long.");
+  } else {
+    alert("")
+  }
 };
 
 loginButton.addEventListener("click", onLoginClick);
