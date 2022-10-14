5783c59e96cc59acdfb5fbdb0d25e33afa3f1a4b
idooru <shere1765@gmail.com>
2022-10-14 22:07:04 +0900

    [수정] 이벤트 사용하기 연습

--- app.js
+++ app.js
@@ -11,6 +11,27 @@ function onLoginSubmit(event) {
   const username = loginInput.value;
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting2.classList.remove(HIDDEN_CLASSNAME);
+}
+
+function changeFontColor() {
+  const fontColor = greeting2.style.color;
+  let newColor;
+
+  switch (fontColor) {
+    case "":
+      newColor = "red";
+      break;
+    case "red":
+      newColor = "blue";
+      break;
+    case "blue":
+      newColor = "red";
+      break;
+  }
+
+  greeting2.style.color = newColor;
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+greeting2.addEventListener("click", changeFontColor);
