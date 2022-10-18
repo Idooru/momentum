2bf196f94b17581c7a8542b95408a2c70a253a2d
idooru <shere1765@gmail.com>
2022-10-18 22:11:33 +0900

    [수정] 배경화면 이미지 순서 변경

--- javascript/background.js
+++ javascript/background.js
@@ -4,4 +4,4 @@ const images = ["img11.jpg", "img12.jpg", "img13.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 bgImage.src = `/img/${chosenImage}`;
-document.body.appendChild(bgImage);
+document.body.prepend(bgImage);
