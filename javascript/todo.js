cff8a06cc0982a90389e61077c7788c1bc768f5e
idooru <shere1765@gmail.com>
2022-10-19 15:56:03 +0900

    [추가] todos 삭제하기 1

--- javascript/todo.js
+++ javascript/todo.js
@@ -17,13 +17,14 @@ function deleteToDo(event) {
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   toDoList.appendChild(li);
 }
 
@@ -31,14 +32,16 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveToDo();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
-
-function sayHello() {}
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
