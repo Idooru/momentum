50290f7be55da72894f8543c35f6d6d460c627cd
idooru <shere1765@gmail.com>
2022-10-19 18:23:21 +0900

    [추가] 날짜 API 사용

--- javascript/weather.js
+++ javascript/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = "7196fafe1803dddb44ff441e30b07bc2";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      city.innerText = data.name;
+    });
 }
 
 function onGeoErr() {
