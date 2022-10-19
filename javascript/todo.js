403afe82e79fbd367f16cea43e59051ac8a5d767
idooru <shere1765@gmail.com>
2022-10-19 14:36:40 +0900

    [수정] toDoList 요소 삭제

--- javascript/todo.js
+++ javascript/todo.js
@@ -2,13 +2,17 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
-const TODO_KEY = "todo";
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
   const button = document.createElement("button");
   button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
   span.innerText = newTodo;
